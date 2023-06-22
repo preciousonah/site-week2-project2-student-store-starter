1285aaed44a2531c17e6957b782386a54a994422
Precious Onah <precious20@meta.com>
2023-06-22 06:03:28 -0700

    Add a product component and import into the App.jsx file, use the useEffect to fetch data from JSON using API, use the useState tom store the data

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,13 +1,13 @@
-
-import * as React from "react"
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from "../Navbar/Navbar"
-import Logo from "../Logo/Logo"
-import Sidebar from "../Sidebar/Sidebar"
-import Home from "../Home/Home"
-import "./App.css"
-import Banner from "../Banner/Banner"
-import Search from "../SearchForm/SearchForm"
+import { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Logo from "../Logo/Logo";
+import Sidebar from "../Sidebar/Sidebar";
+import Home from "../Home/Home";
+import "./App.css";
+import Banner from "../Banner/Banner";
+import Search from "../SearchForm/SearchForm";
+import Products from "../Products/Products"; 
 // import About from "../About/About"
 // import CheckoutForm from "../CheckoutForm/CheckoutForm"
 // import Contact from "../Contact/Contact"
@@ -17,17 +17,30 @@ import Search from "../SearchForm/SearchForm"
 
 
 export default function App() {
-  
+  const [products, setProducts] = useState([]); // State to store the products
+
+  useEffect(() => {
+    // Fetch the API data
+    fetch("https://codepath-store-api.herokuapp.com/store")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); // Check the response data
+      setProducts(data);
+    })
+    .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="app">
       <BrowserRouter>
         <main>
-          {/* YOUR CODE HERE! */}
           <Navbar />
           <Logo />
           <Banner />
-          <Search/>
+          <Search />
+          <Products/>
           <Sidebar />
+          <Products products={products} /> {/* Pass the products state as props */}
           <Home />
         </main>
       </BrowserRouter>
---
+++ student-store-ui/src/components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = ({ product }) => {
+  return (
+    <div>
+      <h3>Product</h3>
+    </div>
+  );
+};
+
+export default Product;
